2554db65c75f549501b1bed2b9e03f852ea685df
wendellGJ <wendell.guimaraes.junior@ice.ufjf.br>
2021-06-27 10:38:15 -0300

    feat: add footer

---
+++ src/utils/components/Footer/index.jsx
@@ -0,0 +1,27 @@
+import { Grid, Typography } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import React from 'react';
+import images from '../../assets/imgs';
+import { useStyles } from './styles';
+
+/**
+ *
+ * @Footer Componente responsável pelo footer da aplicacao
+ * @CriadoEm 19/08/2020
+ */
+export default function Footer() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3} justify="center" alignContent="center" className={classes.containerFooterAbaixo}>
+        <Grid item xs={6}>
+          <img src={images.GithubBig} alt="logo" className={classes.logo} />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography className={classes.txtCompanhia}>Feito por Wendell Guimarães Júnior</Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
