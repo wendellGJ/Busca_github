928456904f870deec994314021659aca0f4d4b64
wendellGJ <wendell.guimaraes.junior@ice.ufjf.br>
2021-06-29 11:34:03 -0300

    fix: atualizado theme do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { Grid } from '@material-ui/core';
+import { Grid, ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
 import './index.css';
 import Routes from './routes';
 import store from './store';
+import appTheme from './theme/index';
 import Footer from './utils/components/Footer/index';
 import Header from './utils/components/Header/index';
-import appTheme from './theme/index';
 
 export default function App() {
   return (
